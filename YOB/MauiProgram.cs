10305a2eab893e61e2efa667639709911c556b05
Nathaniel Bond <nabond251@gmail.com>
2025-06-17 23:30:16 -0400

    Remove current main page

--- YOB/MauiProgram.cs
+++ YOB/MauiProgram.cs
@@ -38,7 +38,6 @@ namespace YOB
             builder.Services.AddSingleton<TagRepository>();
             builder.Services.AddSingleton<SeedDataService>();
             builder.Services.AddSingleton<ModalErrorHandler>();
-            builder.Services.AddSingleton<MainPageModel>();
             builder.Services.AddSingleton<ProjectListPageModel>();
             builder.Services.AddSingleton<ManageMetaPageModel>();
 
