b558d4a3609727116399627015b1418d485dae23
Nathaniel Bond <nabond251@gmail.com>
2025-06-28 21:25:34 -0400

    Scroll to day

--- YOB/Pages/MainPage.xaml.cs
+++ YOB/Pages/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace YOB.Pages
 {
     public partial class MainPage : ContentPage
@@ -6,6 +8,21 @@ namespace YOB.Pages
         {
             InitializeComponent();
             BindingContext = model;
+        }
+
+        private async void CalendarButton_Clicked(object sender, EventArgs e)
+        {
+            var now = DateTime.Now.Date;
+            if (this.BindingContext is MainPageModel pageModel &&
+                pageModel.Projects.FirstOrDefault(
+                    d => DateTime.Parse($"{d.Name}/{now.Year}", CultureInfo.InvariantCulture) >= now)
+                    is Models.Project day &&
+                pageModel.Projects.IndexOf(day) is int index &&
+                index >= 0 &&
+                this.Days[index] is Element item)
+            {
+                await this.DayScroller.ScrollToAsync(item, ScrollToPosition.Start, true);
+            }
         }
     }
 }
